bb96c0757f0d931b995d2e3b27c845f2f73925be
TauriSaar <tauri.saar@voco.ee>
2023-03-04 23:36:15 +0200

    [create_acc #184503892]

--- index.js
+++ index.js
@@ -22,8 +22,35 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     const status = err.statusCode || 500;
     res.status(status).send(err.message);
-})
+});
+
+app.post('/users', (req, res) => {
+    const { email, password } = req.body;
+    const user = { email, password };
+
+    connection.query('INSERT INTO users SET ?', user, (error, results, fields) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Error creating user');
+        } else {
+            console.log(results);
+            res.status(201).send('User created');
+        }
+    });
+});
 
 app.listen(port, () => {
+    const connection = mysql.createConnection({
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_NAME || 'mydatabase',
+    });
+
+    connection.connect((err) => {
+        if (err) throw err;
+        console.log('Connected to MySQL database');
+    });
+
     console.log(`App running. Docs at http://localhost:${port}/docs`);
-})
+});
